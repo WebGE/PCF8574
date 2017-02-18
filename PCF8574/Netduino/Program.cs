5d0744ef1fee2d88d29faeb56252f9d08d706f90
Mariano Philippe <philippemariano@hotmail.fr>
2017-02-18 23:58:44 +0100

    maj

--- PCF8574/Netduino/Program.cs
+++ PCF8574/Netduino/Program.cs
@@ -2,22 +2,22 @@
 using System.Threading;
 using Microsoft.SPOT;
 
-using ToolBoxes;
+using MicroToolsKit.Hardware.IO;
 
 namespace TestNetduinoPCF8574
 {
     public class Program
-    { // Documentation de la classe PCF8574 http://webge.github.io/PCF8574/
+    { 
         public static void Main()
-        { // Pour accéder au bus I2C, relier le PCF8574 au connecteur TWI de la carte Tinkerkit
-            // Paramètres du bus I2C
-            byte addLeds_I2C = 0x38; // Adresse (7 bits) du PCF8574A relié aux Leds
-            Int16 Freq = 100; // Fréquence d'horloge du bus I2C en kHz
+        { 
+            // Pour accéder au bus I2C, relier le PCF8574 au connecteur TWI de la carte Tinkerkit
+            byte SLA = 0x38; // Adresse (7 bits) du PCF8574A relié aux Leds
+            Int16 Frequency = 100; // Fréquence d'horloge du bus I2C en kHz
 
             byte stateLED = 0xFE; // Etat initial des LED. (Un 0 logique => Led éclairée)
 
             // Création d'un objet Leds
-            PCF8574 Leds = new PCF8574(addLeds_I2C, Freq);
+            PCF8574 Leds = new PCF8574(SLA, Frequency);
 
             Leds.Write(stateLED); // Initialisation de l'état des Leds
             Thread.Sleep(500);
---
+++ PCF8574/PCF8574/PCF8574.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace MicroToolsKit
+{
+    namespace Hardware
+    {
+        namespace IO
+        {
+            /// <summary>
+            /// PCF8574 - Remote 8-bit I/O expander for I2C-bus with interrupt class
+            /// </summary>
+            /// <remarks>
+            /// You may have some additional information about this class on http://webge.github.io/PCF8574/
+            /// </remarks>
+            public class PCF8574
+            {
+                /// <summary>
+                /// PCF8574 configuration
+                /// </summary>
+                private I2CDevice.Configuration config;
+
+                private I2CDevice i2cBus;
+
+                const UInt16 TRANSACTIONTIMEOUT = 1000;
+
+                /// <summary>
+                /// 7-bit Slave Adress
+                /// </summary>
+                UInt16 sla;
+
+                /// <summary>
+                /// PCF8574 8-bit I/O expander
+                /// </summary>
+                /// <param name="SLA">PCF8574(@=0x20 to 0x27) or PCF8574A(@=0x38 to 0x3f), 0x38 by default</param>
+                /// <param name="Frequency">100khz to 400kHz, 100kHz by default </param>
+                public PCF8574(UInt16 SLA = 0x38, Int16 Frequency = 100)
+                {
+                    sla = SLA;
+                    config = new I2CDevice.Configuration(SLA, Frequency);
+                }
+
+
+                /// <summary>
+                /// Write byte on PCF8574 port I/O
+                /// </summary>
+                /// <param name="value"></param>
+                public void Write(Byte value)
+                {
+                    byte[] outbuffer = new byte[] { value };
+
+                    I2CDevice.I2CTransaction[] XAction = new I2CDevice.I2CTransaction[] { I2CDevice.CreateWriteTransaction(outbuffer) };
+                    i2cBus = new I2CDevice(config);
+                    int transferred = i2cBus.Execute(XAction, TRANSACTIONTIMEOUT);
+                    i2cBus.Dispose();
+                    if (transferred < (outbuffer.Length))
+                        throw new System.IO.IOException("I2CBus error:" + sla.ToString());
+                }
+
+                /// <summary>
+                /// Read the PCF8574 port I/O 
+                /// </summary>
+                public byte Read()
+                {
+                    byte[] inbuffer = new byte[1];
+
+                    I2CDevice.I2CTransaction[] XAction = new I2CDevice.I2CTransaction[] { I2CDevice.CreateReadTransaction(inbuffer) };
+                    i2cBus = new I2CDevice(config);
+                    int transferred = i2cBus.Execute(XAction, 1000);
+                    i2cBus.Dispose();
+                    if (transferred < (inbuffer.Length))
+                        throw new System.IO.IOException("I2CBus error:" + sla.ToString());
+                    else
+                        return inbuffer[0];
+                }
+            }
+        }
+    }
+}
