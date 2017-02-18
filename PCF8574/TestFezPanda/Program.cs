a5f599c16e3006d809f07e54e2b58e4db893a97d
Mariano Philippe <philippemariano@hotmail.fr>
2017-02-19 00:17:36 +0100

    Ajout nouvelle version 1.4.3.1
    Conforme aux espaces de nom de MicroToolsKit

---
+++ PCF8574/TestFezPanda/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHI.Pins;
+
+using MicroToolsKit.Hardware.IO;
+
+namespace TestFezPanda
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Pour accéder au bus I2C, relier le PCF8574 au connecteur TWI de la carte Tinkerkit
+            byte SLA = 0x38; // Adresse (7 bits) du PCF8574A relié aux Leds
+            Int16 Frequency = 100; // Fréquence d'horloge du bus I2C en kHz
+
+            byte stateLED = 0xFE; // Etat initial des LED. (Un 0 logique => Led éclairée)
+
+            // Création d'un objet Leds
+            PCF8574 Leds = new PCF8574(SLA, Frequency);
+
+            Leds.Write(stateLED); // Initialisation de l'état des Leds
+            Thread.Sleep(500);
+
+            while (true)
+            {
+                // Modification de l'état des LED
+                if (stateLED != 0)
+                {
+                    stateLED = (byte)(stateLED << 1);
+                }
+                else
+                {
+                    stateLED = 0xFF;
+                }
+
+                //Ecriture sur les Leds
+                Leds.Write(stateLED);
+                Debug.Print(stateLED.ToString());
+                Thread.Sleep(500); // Pour la simulation
+            }
+        }
+    }
+}
