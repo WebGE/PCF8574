fcf6f031e0ec424ecec0e955c8f05ea2bd1c1c8a
Mariano Philippe <philippemariano@hotmail.fr>
2017-03-02 00:09:02 +0100

    update

--- PCF8574/TestFezPanda/Program.cs
+++ PCF8574/TestFezPanda/Program.cs
@@ -1,7 +1,13 @@
+#define LCD
+
 using System.Threading;
 using Microsoft.SPOT;
 
 using testMicroToolsKit.Hardware.IO;
+
+#if LCD
+using Microtoolskit.Hardware.Displays;
+#endif
 
 namespace TestFezPanda
 {
@@ -12,6 +18,12 @@ namespace TestFezPanda
             byte state = 0xFE;
 
             PCF8574 Leds = new PCF8574(); // SLA = 0x38, Frequency = 100kHz
+
+#if LCD
+            ELCD162 lcd = new ELCD162("COM1");
+            lcd.Init(); lcd.ClearScreen(); lcd.CursorOff();
+            lcd.PutString("Chaser demo"); lcd.SetCursor(0, 1);
+#endif
 
             Leds.Write(state);
             Thread.Sleep(500);
@@ -30,16 +42,23 @@ namespace TestFezPanda
                 try
                 {
                     Leds.Write(state);
+#if LCD
+                    lcd.SetCursor(0, 1); lcd.PutString("Value = "+ state + "     ");
+#else
                     Debug.Print(state.ToString());
+#endif
                 }
                 catch (System.IO.IOException ex)
                 {
-
+#if LCD
+                    lcd.SetCursor(0, 1); lcd.PutString(ex.Message);
+#else
                     Debug.Print(ex.Message);
+#endif
                 }
                 finally
                 {
-                    Thread.Sleep(500);
+                    Thread.Sleep(1000);
                 }
             }
         }
